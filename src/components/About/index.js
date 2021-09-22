06d9acad72f8f09930738cd3d402b2c03708d2c3
Maame Konadu Assasie-Gymah <maame@Maames-MacBook-Pro.local>
2021-09-22 10:09:50 -0400

    about section and nav added

---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="my-5">
+      <div className="my-2">
+        <div className="profile-img my-5">
+        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+        </div>
+        <p>
+        I am currently a Mechanical Engineer with a Bachelors and Masters degree from Rutgers University. 
+        I am attending the Rutgers University Coding Bootcamp, learning new skills in HTML, CSS and JavaScript. 
+        I am very excited to learn these intersetomng programs to broaden my horizon to become a front end developer 
+        after the bootcamp ends. 
+        I also plan on merging my engineering knowledge and my coding skills to make the world a better place.
+        </p>
+        <p>
+        Coding Rocks!
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
